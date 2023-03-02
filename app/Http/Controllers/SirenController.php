061e4ba8b2765f97494ac4a4da82f7effcc0e4b4
eddymandran <eddy.mandran@gmail.com>
2023-03-02 14:37:46 +0100

    feat(sirenController): update getSiren method and add phpdoc

--- app/Http/Controllers/SirenController.php
+++ app/Http/Controllers/SirenController.php
@@ -5,23 +5,31 @@ namespace App\Http\Controllers;
 use App\Traits\HttpResponses;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class SirenController extends Controller
 {
     use HttpResponses;
 
-
-    protected $apiKey;
+    protected string $apiKey;
 
     public function __construct()
     {
+        // initialise the API key for the INSEE API SIRENE
         $this->apiKey = base64_encode(env('INSEE_CONSUMER_KEY') . ':' . env('INSEE_CONSUMER_SECRET'));
     }
 
-    public function generateToken()
+    /**
+     * Generate a valid access token for the SIRENE API of INSEE.
+     * (subscription required to use this API)
+     *
+     * @return string  access token for the INSEE API SIRENE
+     * @throws GuzzleException
+     */
+    public function generateToken(): string
     {
-
         $client = new Client();
         $result = $client->post('https://api.insee.fr/token', [
             'headers' => [
@@ -35,14 +43,27 @@ class SirenController extends Controller
 
         $result = json_decode($result->getBody());
         return $result->access_token;
-
     }
 
-    public function getSiren($siren)
+    /**
+     * Retrieve information about a company using its SIREN number.
+     *
+     * @param string $siren enterprise SIREN number
+     * @return JsonResponse
+     */
+    public function getSiren(string $siren): JsonResponse
     {
         try {
             // format siren
             $sirenToCheck = str_replace(' ', '', $siren);
+
+            $validator = Validator::make(['siren' => $sirenToCheck], [
+                'siren' => 'required|numeric|digits:9',
+            ]);
+
+            if ($validator->fails()) {
+                return $this->error(null, $validator->errors()->first(), 400);
+            }
 
             //generate token
             $tokenGenerated = $this->generateToken();
@@ -55,12 +76,26 @@ class SirenController extends Controller
                 ],
                 'http_errors' => false,
             ]);
-            $response = json_decode($response->getBody());
-            return $this->success($response, 'Siren found');
 
+            // Check errors return by INSEE API SIRENE
+            if ($response->getStatusCode() !== 200) {
+                return match ($response->getStatusCode()) {
+                    401 => $this->error(null, 'Unauthorized', 401),
+                    403 => $this->error(null, 'Access forbidden', 403),
+                    404 => $this->error(null, 'Siren not found', 404),
+                    429 => $this->error(null, 'Too many requests', 429),
+                    500 => $this->error(null, 'Internal server error', 500),
+                    default => $this->error(null, 'Unknown error', 500),
+                };
+            } else {
+                $dataFetch = json_decode($response->getBody());
+                return $this->success($dataFetch->uniteLegale, 'Siren found');
+            }
         } catch (GuzzleException $error) {
             Log::error($error);
             return $this->error(null, $error->getMessage(), 500);
         }
     }
 }
+
+
