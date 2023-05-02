89e355cb8a01b66ce4fefbdb9136d5abfed4d375
eddymandran <eddy.mandran@gmail.com>
2023-05-02 23:05:37 +0200

    fix(categoryController): fix store and update method

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,6 +15,7 @@ class CategoryController extends Controller
 
     private const CATEGORIES_LIST = "Categories List";
 
+    private const BEER_ICON = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><path d='M32 64c0-17.7 14.3-32 32-32H352c17.7 0 32 14.3 32 32V96h51.2c42.4 0 76.8 34.4 76.8 76.8V274.9c0 30.4-17.9 57.9-45.6 70.2L384 381.7V416c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V64zM384 311.6l56.4-25.1c4.6-2.1 7.6-6.6 7.6-11.7V172.8c0-7.1-5.7-12.8-12.8-12.8H384V311.6zM160 144c0-8.8-7.2-16-16-16s-16 7.2-16 16V368c0 8.8 7.2 16 16 16s16-7.2 16-16V144zm64 0c0-8.8-7.2-16-16-16s-16 7.2-16 16V368c0 8.8 7.2 16 16 16s16-7.2 16-16V144zm64 0c0-8.8-7.2-16-16-16s-16 7.2-16 16V368c0 8.8 7.2 16 16 16s16-7.2 16-16V144z'/></svg>";
 
     /**
      * Get all categories with sub_category = Establishment || All
@@ -77,8 +78,6 @@ class CategoryController extends Controller
 
     public function store(Request $request): JsonResponse
     {
-        $beerIcon = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><path d='M32 64c0-17.7 14.3-32 32-32H352c17.7 0 32 14.3 32 32V96h51.2c42.4 0 76.8 34.4 76.8 76.8V274.9c0 30.4-17.9 57.9-45.6 70.2L384 381.7V416c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V64zM384 311.6l56.4-25.1c4.6-2.1 7.6-6.6 7.6-11.7V172.8c0-7.1-5.7-12.8-12.8-12.8H384V311.6zM160 144c0-8.8-7.2-16-16-16s-16 7.2-16 16V368c0 8.8 7.2 16 16 16s16-7.2 16-16V144zm64 0c0-8.8-7.2-16-16-16s-16 7.2-16 16V368c0 8.8 7.2 16 16 16s16-7.2 16-16V144zm64 0c0-8.8-7.2-16-16-16s-16 7.2-16 16V368c0 8.8 7.2 16 16 16s16-7.2 16-16V144z'/></svg>";
-
 
         $request->validate([
             'category_details.sub_category' => [
@@ -89,21 +88,18 @@ class CategoryController extends Controller
             'category_details.label' => 'required|string',
             'category_details.icon' => 'nullable|string',
         ], [
-            'category_details.sub_category.in' => 'La sous categorie doit être Establishment, Event ou All',
-            'category_details.sub_category.required' => 'La sous categorie est requise',
-            'category_details.label.required' => 'Le label est requis',
-            'category_details.icon.string' => 'L\'icone doit être une chaine de caractères',
-        ]);
+                'category_details.sub_category.in' => 'La sous categorie doit être Establishment, Event ou All',
+                'category_details.sub_category.required' => 'La sous categorie est requise',
+                'category_details.label.required' => 'Le label est requis',
+                'category_details.icon.string' => 'L\'icone doit être une chaine de caractères',
+            ]);
 
-        if ($request->input('category_details.icon') === null) {
-            $request->merge(['category_details.icon' => $beerIcon]);
-        }
-
+        
         $newCategory = array(
             'sub_category' => $request->input('category_details.sub_category'),
             'label' => $request->input('category_details.label'),
             'icon' => $request
-                ->input('category_details.icon') === null ? $beerIcon : $request->input('category_details.icon'),
+                ->input('category_details.icon') === null ? self::BEER_ICON: $request->input('category_details.icon'),
         );
 
         $newCategory = json_encode($newCategory);
@@ -149,13 +145,24 @@ class CategoryController extends Controller
             'category_details.label' => 'required|string',
             'category_details.icon' => 'nullable|string',
         ], [
-            'category_details.sub_category.in' => 'La sous categorie doit être Establishment, Event ou All',
-            'category_details.sub_category.required' => 'La sous categorie est requise',
-            'category_details.label.required' => 'Le label est requis',
-            'category_details.icon.string' => 'L\'icone doit être une chaine de caractères',
-        ]);
+                'category_details.sub_category.in' => 'La sous categorie doit être Establishment, Event ou All',
+                'category_details.sub_category.required' => 'La sous categorie est requise',
+                'category_details.label.required' => 'Le label est requis',
+                'category_details.icon.string' => 'L\'icone doit être une chaine de caractères',
+            ]);
 
-        $category->category_details = json_encode($request->input('category_details'));
+               
+        $updateCategory = array(
+            'sub_category' => $request->input('category_details.sub_category'),
+            'label' => $request->input('category_details.label'),
+            'icon' => $request
+                ->input('category_details.icon') === null ? self::BEER_ICON : $request->input('category_details.icon'),
+        );
+
+        $updateCategory = json_encode($updateCategory);
+
+
+        $category->category_details =  $updateCategory;
         $category->save();
 
         return $this->success($category, "Category updated successfully");
@@ -208,7 +215,8 @@ class CategoryController extends Controller
             ->select(
                 'categories.category_id',
                 'categories.category_details',
-                DB::raw('COUNT(categories_events.category_id) as total_cate'))
+                DB::raw('COUNT(categories_events.category_id) as total_cate')
+            )
             ->groupBy('categories.category_id')
             ->orderBy('total_cate', 'desc')
             ->skip(0)
