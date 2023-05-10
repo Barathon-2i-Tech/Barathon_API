0b067da9896a66d0d7dbfac37dea9bfcf0e0b44d
Pella0 <67748582+Pella0@users.noreply.github.com>
2023-05-10 20:13:22 +0200

    fix(profile): split

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -20,11 +20,11 @@ class OwnerController extends Controller
     private const STRINGVALIDATION = 'required|string|max:255';
     private const OWNERNOTFOUND = 'Owner not found';
     private const USERNOTFOUND = 'User not found';
-    private const AVATARURL_PART1 = 'https://img.freepik.com/free-photo/tasty-american-';
-    private const AVATARURL_PART2 = 'beer-arrangement_23-2148907580.jpg?w=740&t=st=';
-    private const AVATARURL_PART3 = '1683116391~exp=1683116991~';
-    private const AVATARURL_PART4 = 'hmac=584918e27d013319c35203ce268841f480637965556343c6173885ba806453f2';
-    private const AVATARURL = self::AVATARURL_PART1 . self::AVATARURL_PART2 . self::AVATARURL_PART3 . self::AVATARURL_PART4;
+    private const AVATARURL_P1 = 'https://img.freepik.com/free-photo/tasty-american-';
+    private const AVATARURL_P2 = 'beer-arrangement_23-2148907580.jpg?w=740&t=st=';
+    private const AVATARURL_P3 = '1683116391~exp=1683116991~';
+    private const AVATARURL_P4 = 'hmac=584918e27d013319c35203ce268841f480637965556343c6173885ba806453f2';
+    private const AVATARURL = self::AVATARURL_P1 . self::AVATARURL_P2 . self::AVATARURL_P3 . self::AVATARURL_P4;
     private const PHONEVALIDATION = ['regex:/^([0-9\s\-\+\(\)]*)$/', 'min:10'];
 
     /**
