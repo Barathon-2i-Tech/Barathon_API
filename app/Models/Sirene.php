d981f19acdfc16ffc95ab5b1823ca992070d4e22
eddymandran <eddy.mandran@gmail.com>
2023-05-23 17:20:05 +0200

    feat(sirene): add connection to model

--- app/Models/Sirene.php
+++ app/Models/Sirene.php
@@ -8,6 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Sirene extends Model
 {
     use HasFactory;
+
+    /**
+     * connection to 2nd database
+     */
+    protected $connection = 'pgsql_db_sirene';
+
     /**
      * The table associated with the model.
      *
