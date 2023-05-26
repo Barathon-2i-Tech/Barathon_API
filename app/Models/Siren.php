e154d4035056330fcf6157989c32552a754615ac
eddymandran <eddy.mandran@gmail.com>
2023-05-26 22:38:06 +0200

    feat(inseecontroller): create siren model

---
+++ app/Models/Siren.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Siren extends Model
+{
+    use HasFactory;
+
+    /**
+     * connection to 2nd database
+     */
+    protected $connection = 'pgsql_db_sirene';
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'siren';
+
+}
