6b916df7710ee8292991bfdb76ae3aec382e7fce
eddymandran <eddy.mandran@gmail.com>
2023-05-29 16:11:37 +0200

    feat(migration): fix sonarcloud error(empty statement)

--- database/migrations/2023_05_26_160714_create_siren_table.php
+++ database/migrations/2023_05_26_160714_create_siren_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->boolean('unitePurgeeUniteLegale')->nullable();
             $table->date('dateCreationUniteLegale')->nullable();
             $table->string('sigleUniteLegale', 20)->nullable();
-            $table->string('sexeUniteLegale', 4)->nullable();;
+            $table->string('sexeUniteLegale', 4)->nullable();
             $table->string('prenom1UniteLegale', 20)->nullable();
             $table->string('prenom2UniteLegale', 20)->nullable();
             $table->string('prenom3UniteLegale', 20)->nullable();
@@ -28,12 +28,12 @@ return new class extends Migration
             $table->string('pseudonymeUniteLegale', 100)->nullable();
             $table->string('identifiantAssociationUniteLegale', 10)->nullable();
             $table->string('trancheEffectifsUniteLegale', 2)->nullable();
-            $table->string('anneeEffectifsUniteLegale', 4)->nullable();;
+            $table->string('anneeEffectifsUniteLegale', 4)->nullable();
             $table->timestampTz('dateDernierTraitementUniteLegale')->nullable();
-            $table->integer('nombrePeriodesUniteLegale')->nullable();;
-            $table->string('categorieEntreprise', 3)->nullable();;
-            $table->string('anneeCategorieEntreprise', 4)->nullable();;
-            $table->date('dateDebut')->nullable();;
+            $table->integer('nombrePeriodesUniteLegale')->nullable();
+            $table->string('categorieEntreprise', 3)->nullable();
+            $table->string('anneeCategorieEntreprise', 4)->nullable();
+            $table->date('dateDebut')->nullable();
             $table->string('etatAdministratifUniteLegale', 1)->nullable();
             $table->string('nomUniteLegale', 100)->nullable();
             $table->string('nomUsageUniteLegale', 100)->nullable();
@@ -43,10 +43,10 @@ return new class extends Migration
             $table->string('denominationUsuelle3UniteLegale', 70)->nullable();
             $table->string('categorieJuridiqueUniteLegale', 4)->nullable();
             $table->string('activitePrincipaleUniteLegale', 6)->nullable();
-            $table->string('nomenclatureActivitePrincipaleUniteLegale', 8)->nullable();;
+            $table->string('nomenclatureActivitePrincipaleUniteLegale', 8)->nullable();
             $table->string('nicSiegeUniteLegale', 5)->nullable();
             $table->string('economieSocialeSolidaireUniteLegale', 1)->nullable();
-            $table->string('societeMissionUniteLegale', 1)->nullable();;
+            $table->string('societeMissionUniteLegale', 1)->nullable();
             $table->string('caractereEmployeurUniteLegale', 1)->nullable();
         });
     }
