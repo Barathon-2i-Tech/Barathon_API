5be4636b713074335b4c9b825a64ba9eca5913f4
Pella0 <67748582+Pella0@users.noreply.github.com>
2023-06-04 20:52:52 +0200

    fix(delete-of-event): fix condition for delete event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -264,7 +264,7 @@ class EventController extends Controller
         }
 
         // Check if the current authenticated user is the owner of the establishment or an administrator
-        if ($user->owner_id !== $event->establishment->owner_id && $user->owner_id !== null || $user->administrator_id === null) {
+        if (!($user->owner_id === $event->establishment->owner_id || $user->administrator_id !== null)) {
             return $this->error(null, "Unauthorized", 401);
         }
 
